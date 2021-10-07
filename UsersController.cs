488883ce73affe79a7218728409a7eb1f60f7aca
3209219422 <61813891+3209219422@users.noreply.github.com>
2021-10-07 23:45:14 +0800

    Add files via upload

---
+++ UsersController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Service
+{
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        // GET: api/<controller>
+        [HttpGet]
+       public ActionResult Get()
+        {
+            return Json(DAL.UserInfo.Instance.GetCount());
+        }
+        [HttpPut("{username}")]
+        public ActionResult getUser(string username)
+        {
+            var result = DAL.UserInfo..Instance.GetModel(username);
+            if (result != null)
+                return Json(Result.Ok(result));
+            else
+                return Json(Result.Err("用户名不存在"));
+        }
+    }
+}
